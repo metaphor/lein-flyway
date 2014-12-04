8e0054e3876a4e4bff8898f3b9d2c098c0b9c2f1
Metaphor <zhiheng.li.metaphor@gmail.com>
2014-12-04 16:57:38 +0800

    eval migrate task in project context

--- src/leiningen/flyway_migrate.clj
+++ src/leiningen/flyway_migrate.clj
@@ -1,9 +1,12 @@
 (ns leiningen.flyway-migrate
-  (:require [flyway.flyway :refer [flyway]]
-            [leiningen.core.eval :refer [eval-in-project]]))
+  (:require [leiningen.core.eval :refer [eval-in-project]]))
 
 (defn flyway-migrate [project]
   (let [config (:flyway project)]
-    (-> (flyway config) .migrate)))
+    (eval-in-project
+     (update-in project [:dependencies] conj ['lein-flyway "0.1.0-SNAPSHOT"])
+     `(do
+        (-> (flyway.flyway/flyway ~config) .migrate))
+     '(require 'flyway.flyway))))
 
 
