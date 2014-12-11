5459c37e38cdb556c0e71a45b57811c94454c2c0
Metaphor <zhiheng.li.metaphor@gmail.com>
2014-12-11 22:18:43 +0800

    update project description

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject lein-flyway "0.1.0-SNAPSHOT"
-  :description "Leiningen Plugin for Flyway"
-  :url "http://example.com/FIXME"
+(defproject lein-flyway "3.1-SNAPSHOT"
+  :description "Leiningen Plugin for Database Migration Tool Flyway"
+  :url "https://github.com/metaphor/lein-flyway"
   :min-lein-version "2.0.0"
   :eval-in :leiningen
 
   :dependencies [[org.flywaydb/flyway-core "3.1"]]
 
-  :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.34"]]}}
-  )
+  :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.34"]]}})
