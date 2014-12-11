a5902ff4ebd2cbc412eefcba7d5a00d282a3b0be
Metaphor <zhiheng.li.metaphor@gmail.com>
2014-12-11 22:29:13 +0800

    update project meta

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-flyway "3.1-SNAPSHOT"
+(defproject com.github.metaphor/lein-flyway "3.1-SNAPSHOT"
   :description "Leiningen Plugin for Database Migration Tool Flyway"
   :url "https://github.com/metaphor/lein-flyway"
   :min-lein-version "2.0.0"
