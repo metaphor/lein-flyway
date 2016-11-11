23d1dab4e4328caba8c89ed7c30bc1d9b4be1d75
mahinshaw <mahinshaw@gmail.com>
2016-11-11 10:40:03 -0800

    Use config files for flyway.

--- project.clj
+++ project.clj
@@ -2,16 +2,19 @@
   :description "Leiningen Plugin for Database Migration Tool Flyway"
   :url "https://github.com/metaphor/lein-flyway"
   :license {:name "Eclipse Public License - v 1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :eval-in :leiningen
 
-  :dependencies [[org.flywaydb/flyway-core "3.2.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.flywaydb/flyway-core "4.0.3"]]
 
-  :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.36"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+                                  [mysql/mysql-connector-java "5.1.36"]]
+                   :plugins      [[com.jakemccrary/lein-test-refresh "0.15.0"]]
+                   }}
 
-  :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
-
+  :test-refresh {:changes-only true}
 
   :scm {:url "git@github.com:metaphor/lein-flyway.git"}
   :pom-addition [:developers [:developer
---
+++ test/flyway/flyway_test.clj
@@ -0,0 +1,47 @@
+(ns flyway.flyway-test
+  (:require [clojure.string :as str]
+            [clojure.test :as t :refer [deftest is are]]
+            [flyway.flyway :as sut])
+  (:import java.util.Properties))
+
+(def edn-file "test/config/config.edn")
+(def conf-file "test/config/flyway.conf")
+
+(deftest get-edn-config
+  (is (not (nil?
+            (sut/get-edn-config edn-file))))
+  (is (= "Config path does not exist: \n"
+         (with-out-str (sut/get-edn-config "")))))
+
+(deftest get-conf-config
+  (is (not (nil?
+            (sut/get-conf-config conf-file))))
+  (is (= "Config path does not exist: \n"
+         (with-out-str (sut/get-conf-config "")))))
+
+(deftest make-flyway-prop
+  (is (= "flyway.test" (sut/make-flyway-prop :test)))
+  (is (= "flyway.testThisThing" (sut/make-flyway-prop :test-this-thing)))
+  (is (= "flyway.testThisThing" (sut/make-flyway-prop :test_this_thing)))
+  (is (= "flyway.testThisThing" (sut/make-flyway-prop :testThisThing))))
+
+(def config-map {:user                      "root"
+                 :locations                 ["classpath:/migration1" "migrations2" "filesystem:/sql-migrations"]
+                 :encoding                  "UTF-8"
+                 :placeholders              {"token1" "token1-value"
+                                             :token2 "token2-value"}
+
+                 ;; invalid key
+                 :improper-key              true})
+(def properties (sut/make-properties config-map))
+
+(deftest make-properties
+  (is (instance? Properties properties))
+  (is (not (.getProperty properties "flyway.improperKey")))
+  ;; the placeholders map will create 2 keys.
+  (is (= 5 (count properties)))
+  (is (= (:user config-map) (.getProperty properties "flyway.user")))
+  (is (= (:encoding config-map) (.getProperty properties "flyway.encoding")))
+  (is (= (str/join "," (:locations config-map)) (.getProperty properties "flyway.locations")))
+  (is (= (get-in config-map [:placeholders "token1"]) (.getProperty properties "flyway.placeholders.token1")))
+  (is (= (get-in config-map [:placeholders :token2]) (.getProperty properties "flyway.placeholders.token2"))))
