03eaea544203f5b3eeca19511652a378ffa42af5
Yan Qian <qianyan@thoughtworks.com>
2017-08-23 23:36:38 +0800

    fix ClassNotFoundException: org.apache.commons.io.FilenameUtils. Refer to "Inside the eval-in-project call the project's own classpath will be active and Leiningen's own internals and plugins will not be available."

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :eval-in :leiningen
+  :eval-in-leiningen true
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.flywaydb/flyway-core "4.0.3"]]
+  :dependencies [[org.flywaydb/flyway-core "4.0.3"]
+                 [commons-io "2.4"]]
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [mysql/mysql-connector-java "5.1.36"]]
